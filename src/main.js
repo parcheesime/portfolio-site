9628149f31d5e312daa0d97680e8714475ef6667
Aletia Trepte <aletia.trepte@gmail.com>
2026-06-10 17:22:47 -0700

    Refactor portfolio into modular feature architecture

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import { initBackToTop } from "./features/back-to-top.js";
 import { initFunFacts } from "./features/fun-facts.js";
 import { initJourney } from "./features/journey.js";
 import { incrementScore } from "./features/score-system.js";
+import { createQuestSystem } from "./features/quest-system.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const quest = createQuestSystem();
+
     initModeToggle();
     initBackToTop();
 
-    initFunFacts(() => { });
-    initJourney(() => { }, incrementScore);
+    initFunFacts(quest.updateQuest);
+    initJourney(quest.updateQuest, incrementScore);
 });
